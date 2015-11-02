69c1385a34d1bb5a5d9c38cbf345e29aff695271
Ryan Johnson <rjohnson4444@live.com>
2015-11-01 17:35:37 -0700

    Improved model tests for person and company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 RSpec.describe Company, type: :model do
   let(:company) { Company.new(name: "Chase") }
 
-  xit "is valid" do
+  it "is valid" do
+    expect(company).to be_valid
+  end
+
+  it "is invalid without a name" do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
+
+  it "responds with its phone numbers after they're created" do
+    phone_number = company.phone_numbers.build(number: '333-4444')
+    expect(phone_number.number).to eq('333-4444')
   end
 end
